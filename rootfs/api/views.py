47820557071cddc45526db5a5222c59c1606af6f
lijianguo <lijianguo@zhumengyuan.com>
2021-07-28 16:54:53 +0800

    add login done page

--- rootfs/api/views.py
+++ rootfs/api/views.py
@@ -142,3 +142,6 @@ class UserEmailView(NormalUserViewSet):
 
     def get_object(self):
         return self.request.user
+
+class LoginDoneView(TemplateView):
+    template_name = 'registration/Heroku_Login.html'
