adf3fa2d8142c9ed87223332276f2e76439e6095
lijianguo <lijianguo@zhumengyuan.com>
2021-07-28 16:00:39 +0800

    up CustomOAuth2Validator

--- rootfs/api/serializers.py
+++ rootfs/api/serializers.py
@@ -36,6 +36,7 @@ class CustomOAuth2Validator(OAuth2Validator):
     def get_additional_claims(self, request):
         claims = super().get_additional_claims(request)
         claims["id"] = request.user.id
+        claims["name"] = request.user.username
         claims["username"] = request.user.username
         claims["email"] = request.user.email
         claims["first_name"] = request.user.first_name
